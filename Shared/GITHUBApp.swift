be646adf503554b431b7127184dc16d7a828a858
Alyssa Nolen <alyssanolen23@gmail.com>
2022-07-09 01:10:21 -0500

    Initial Commit

---
+++ Shared/GITHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITHUBApp.swift
+//  Shared
+//
+//  Created by Alyssa Nolen on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GITHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
